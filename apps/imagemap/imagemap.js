7a4a4ae3550d4af73a6252262873b96b686fe58d
Endy Tjahjono <endy_c@yahoo.com>
2013-06-08 20:55:28 +0700

    add angular app html image map helper

---
+++ apps/imagemap/imagemap.js
@@ -0,0 +1,137 @@
+"use strict";
+
+angular
+.module("Globals", [])
+.constant("Settings", {
+    //
+});
+
+angular
+.module("ImageMap", ["Globals"])
+.config(["$routeProvider", "Settings", function ($routeProvider, Settings) {
+    $routeProvider
+    .when(
+        "/", { controller: "MainCtrl", templateUrl: "imagemap.html" }
+    )
+    .otherwise(
+        { redirectTo: "/" }
+    );
+}]);
+
+angular
+.module("ImageMap")
+.controller("MainCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
+    $scope.data = {
+        imagepath: "",
+        areas: []
+    }
+    $scope.image = {
+        imagepath: "",
+        width: 0,
+        height: 0
+    }
+    $scope.loadimage = function () {
+        var img = new Image();
+        img.src = $scope.image.imagepath;
+        $scope.image.width = img.width;
+        $scope.image.height = img.height;
+        $scope.data.imagepath = $scope.image.imagepath;
+    }
+    $scope.currentarea = null;
+    $scope.newarea = function () {
+        $scope.currentarea = {
+            shape: "poly",
+            alt: "",
+            href: "",
+            coordinates: []
+        };
+        $scope.data.areas.push($scope.currentarea);
+    }
+    $scope.loadarea = function (a) {
+        $scope.currentarea = a;
+    }
+    $scope.delarea = function (a) {
+        if ($scope.currentarea === a) {
+            $scope.currentarea = null;
+        }
+        $scope.data.areas.splice($scope.data.areas.indexOf(a), 1);
+    }
+    $scope.areaiscurrent = function (a) {
+        return (a === $scope.currentarea);
+    }
+    var getcoordinate = function (event) {
+        if ("offsetX" in event) {
+            return { x: event.offsetX, y: event.offsetY };
+        } else if ("layerX" in event) {
+            return { x: event.layerX, y: event.layerY };
+        }
+    }
+    $scope.clickimage = function (event) {
+        if ($scope.currentarea != null) {
+            var c = getcoordinate(event);
+            $scope.currentarea.coordinates.push(c);
+        }
+    }
+    $scope.delcoordinate = function (c) {
+        $scope.currentarea.coordinates.splice($scope.currentarea.coordinates.indexOf(c), 1);
+    }
+    $scope.xytos = function (ca) {
+        return _.chain(ca)
+            .map(function (c) { return [c.x, c.y] })
+            .reduce(function (t, i) { return t.concat(i) }, [])
+            .join(",")
+            .value();
+    };
+    $scope.getresultstring = function () {
+        var result = ''
+            + '<img src="' + $scope.data.imagepath + '" usemap="#themap" />'
+            + '\n'
+            + '<map name="themap">';
+
+        for (var i = 0; i < $scope.data.areas.length; i++) {
+            result += '\n'
+                + '<area '
+                + 'shape="' + $scope.data.areas[i].shape + '" '
+                + 'alt="' + $scope.data.areas[i].alt + '" '
+                + 'href="' + $scope.data.areas[i].href + '" '
+                + 'coords="' + $scope.xytos($scope.data.areas[i].coordinates) + '" '
+                + '/>';
+        }
+
+        result += '\n'
+            + '</map>';
+
+        return result;
+    }
+}]);
+
+angular
+.module("ImageMap")
+.directive("mapresult", [function () {
+    return {
+        scope: {
+            mapdata: "="
+        },
+        template: ''
+            + '<img ng-src="{{mapdata.imagepath}}" usemap="#themap" />'
+            + '<map name="themap">'
+            + '<area '
+            + 'ng-repeat="x in mapdata.areas" '
+            + 'shape="{{x.shape}}" '
+            + 'alt="{{x.alt}}" '
+            + 'href="{{x.href}}" '
+            + 'coords="{{xytos(x.coordinates)}}" '
+            + '/>'
+            + '</map>'
+            + '',
+        link: function (scope, element, attributes) {
+            scope.xytos = function (ca) {
+                return _.chain(ca)
+                    .map(function (c) { return [c.x, c.y] })
+                    .reduce(function (t, i) { return t.concat(i) }, [])
+                    .join(",")
+                    .value();
+            };
+        }
+    }
+}]);
