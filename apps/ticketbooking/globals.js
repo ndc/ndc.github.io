0f1132d8dad0601cb9c35de234d7801f0683b8f9
Endy Tjahjono <endy_c@yahoo.com>
2013-10-29 02:28:38 -0400

    CDN links must be changed to HTTPS so apps will run on cloud9

--- apps/ticketbooking/globals.js
+++ apps/ticketbooking/globals.js
@@ -3,7 +3,7 @@
 angular
 .module("Globals", [])
 .constant("Settings", {
-    BookingURL: "http://uspcahharhjy5eb4.apphb.com/blitz",
+    BookingURL: "https://uspcahharhjy5eb4.apphb.com/blitz",
     PartnerCode: "BLITZAPI",
     PartnerKey: "5A581E09-F6EA-410F-9D66-84B74E40971D"
 });
