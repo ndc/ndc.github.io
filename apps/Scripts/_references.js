09bc3e2230a121cb0fc6297628fd0b254c84c1b4
Endy Tjahjono <endy.tjahjono@verint.com>
2018-03-31 20:24:08 +0700

    Removed visual studio files.

--- apps/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="angular.js" />
-/// <reference path="angular-route.js" />
-/// <reference path="underscore.js" />
-/// <reference path="moment.js" />
-/// <reference path="numeral.js" />
-/// <reference path="ui-bootstrap-tpls-0.6.0.js" />
