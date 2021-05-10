3323cc7be8c2a4f002c20c0a13e397168090a293
Endy Tjahjono <endy.tjahjono@verint.com>
2021-05-10 21:17:26 +0700

    Added navbar.

--- apps/selectshowvuempa/app.js
+++ apps/selectshowvuempa/app.js
@@ -1,5 +1,18 @@
 export default {
     template: `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">NNDDCC</a>
+    <div>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item" :class="{active: $route.name == 'selectshow'}">
+                <router-link :to="{name: 'selectshow'}" class="nav-link">
+                    Select Show
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</nav>
+<br />
 <router-view />
 `,
     components: {
