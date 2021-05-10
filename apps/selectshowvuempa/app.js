747b48b94881e90f265f40c52895df1b8838eabe
Endy Tjahjono <endy.tjahjono@verint.com>
2021-05-10 09:50:40 +0700

    Separated app from main.

---
+++ apps/selectshowvuempa/app.js
@@ -0,0 +1,10 @@
+import SelectShow from './selectshow.js'
+
+export default {
+    template: `
+<SelectShow></SelectShow>
+`,
+    components: {
+        SelectShow
+    }
+}
--- apps/selectshowvuempa/main.js
+++ apps/selectshowvuempa/main.js
@@ -1,11 +1,4 @@
-import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3.0.11/dist/vue.esm-browser.js'
-import SelectShow from './selectshow.js'
+import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3.0.11/dist/vue.esm-browser.js'
+import App from './app.js'
 
-Vue.createApp({
-    template: `
-<SelectShow></SelectShow>
-`,
-    components: {
-        SelectShow
-    }
-}).mount("#app");
+createApp(App).mount("#app");
